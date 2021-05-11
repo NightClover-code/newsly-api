98150692384f41b1c3fdbf36a5ae1a42afe57a07
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-05-11 22:18:56 +0200

    removing article resolver and adding more params to articles query

--- schema/resolvers.ts
+++ schema/resolvers.ts
@@ -5,23 +5,15 @@ import newsAPI from '../utils';
 export const resolvers = {
   Query: {
     articles: async () => {
-      const { data } = await newsAPI.get('/everything', {
+      const { data } = await newsAPI.get('/top-headlines', {
         params: {
-          domains: 'cnbc.com',
+          category: 'general',
+          country: 'us',
           apiKey: process.env.API_KEY,
           pageSize: 9,
         },
       });
       return data.articles;
-    },
-    article: async () => {
-      const { data } = await newsAPI.get('/top-headlines', {
-        params: {
-          country: 'us',
-          apiKey: process.env.API_KEY,
-        },
-      });
-      return data.articles[0];
     },
   },
 };
